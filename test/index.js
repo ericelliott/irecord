667960eb21d0a0a218b3bd41ba5b2ee9ffe06bb4
Eric Elliott <support@paralleldrive.com>
2015-06-01 15:47:52 -0600

    Pre-commit hook fix. Lots of updates.

---
+++ test/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import test from 'tape';
+
+test('Tests run', (assert) => {
+  assert.pass('Tests run');
+  assert.end();
+});
+
+test('<Add your tests>', (assert) => {
+  assert.fail('You need to define some tests.');
+});
