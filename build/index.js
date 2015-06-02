47cb2449a5aea47f38c977669d28b7351bfad588
Eric Elliott <support@paralleldrive.com>
2015-06-01 23:19:51 -0600

    [#1] Ugly hack to fix npm require.

---
+++ build/index.js
@@ -0,0 +1,77 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _immutable = require('immutable');
+
+var _immutable2 = _interopRequireDefault(_immutable);
+
+var _dpath = require('dpath');
+
+var _dpath2 = _interopRequireDefault(_dpath);
+
+var _events = require('events');
+
+var _events2 = _interopRequireDefault(_events);
+
+var _stampit = require('stampit');
+
+var _stampit2 = _interopRequireDefault(_stampit);
+
+var _rx = require('rx');
+
+var _rx2 = _interopRequireDefault(_rx);
+
+var irecord = function irecord(obj) {
+  var instance = undefined;
+
+  var history = [];
+
+  var state = function state() {
+    return history[history.length - 1];
+  };
+
+  var update = function update(value) {
+    if (state() === value) return;
+
+    var previous = state();
+
+    history.push(value);
+    instance.emit('change', { value: value, previous: previous });
+  };
+
+  history.push(_immutable2['default'].Map(obj));
+
+  instance = _stampit2['default'].convertConstructor(_events2['default'].EventEmitter).methods({
+    get: function get(key) {
+      return state().getIn((0, _dpath2['default'])(key));
+    },
+    set: function set(key, val) {
+      update(state().setIn((0, _dpath2['default'])(key), val));
+      return this;
+    },
+    remove: function remove(key) {
+      update(state().removeIn((0, _dpath2['default'])(key)));
+      return this;
+    },
+    toJS: function toJS() {
+      return state().toJS();
+    }
+  }).enclose(function () {
+    var source = _rx2['default'].Observable.fromEvent(this, 'change', function (ev) {
+      return ev[0];
+    });
+
+    this.subscribe = source.subscribe.bind(source);
+  }).create();
+
+  return instance;
+};
+
+exports['default'] = irecord;
+module.exports = exports['default'];
+//# sourceMappingURL=index.js.map
